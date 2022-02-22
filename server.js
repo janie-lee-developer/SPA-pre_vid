d92962aca8fbd584ed37643cfb71ffc958413e16
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-02-21 21:24:02 -0500

    set up

---
+++ server.js
@@ -0,0 +1,21 @@
+const { syncAndSeed, models: { User, Car, Sale}, conn } = require('./db');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+app.use('/api', require('./api'));
+
+const init = async() => {
+    try{
+        await syncAndSeed();
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`listening on port ${port}`));
+    }
+    catch(ex){
+        console.log(ex);
+    }
+};
+
+init();
